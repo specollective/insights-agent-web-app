ec1576e3782e2a6d6878d8c39c6c8f7f84ffbc2a
Joe Torreggiani <joetorreggiani@gmail.com>
2022-11-06 20:12:29 -0500

    Add functionality to mirage server.

--- src/mirage.js
+++ src/mirage.js
@@ -1,23 +1,35 @@
-import { createServer } from 'miragejs';
+import { createServer, Response } from 'miragejs';
 import { API_URL } from 'constants/urls';
 
-export default function () {
+export default function makeMirageServer() {
   createServer({
     routes() {
       this.isAuthenticated = false;
       this.urlPrefix = API_URL;
 
       this.post('/send_magic_link', (schema, request) => {
-        console.log('send_magic_link');
+        console.log(
+          'send_magic_link',
+          'http://localhost:3000/confirmation/OTP/TOKEN',
+        );
 
         return {
           id: 1,
         };
       });
 
-      this.post('/confirm_magic_link', (schema, request) => ({
-        id: 1,
-      }));
+      this.post('/confirm_magic_link', (schema, request) => {
+        const body = JSON.parse(request.requestBody);
+        this.isAuthenticated = true;
+
+        if (body.otp === 'OTP' && body.token === 'TOKEN') {
+          return {
+            id: 1,
+          };
+        } else {
+          return new Response(401, { message: 'Invalid OTP' });
+        }
+      });
 
       this.post('/surveys', (schema, request) => {
         const attrs = JSON.parse(request.requestBody)
@@ -28,9 +40,15 @@ export default function () {
         }
       });
 
-      this.get(`${API_URL}/current_user`, (schema, request) => ({
-        id: 1,
-      }));
+      this.get(`${API_URL}/current_user`, (schema, request) => {
+        if (this.isAuthenticated) {
+          return {
+            id: 1,
+          }
+        } else {
+          return new Response(401, { message: 'Not authenticated' });
+        }
+      });
     },
   });
 }
