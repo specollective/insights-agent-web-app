151f93611c595987a9ec946fa563e09b8160860e
najwanan <66931010+najwanan@users.noreply.github.com>
2022-12-01 10:51:39 -0800

    updating download instructions component and download button

--- src/components/elements/Instructions.jsx
+++ src/components/elements/Instructions.jsx
@@ -4,12 +4,23 @@ export default function Instructions() {
   return (
     <>
       <ol>
-        {loremIpsum({ p:5, avgSentencesPerParagraph: 2 }).map(text => (
-          <li className='text' key={text}>
-            {text}
-          </li>
-        ))}
+        {/*add numbers*/}
+        <li>Click Insights Agent icon on your desktop</li>
+        <li>Enter your phone number that you used to sign up for this study</li>
+        <li>
+          You will receive a 6 digit verification code via text to that phone
+          number provided. Enter that code on the prompt
+        </li>
+        <li>
+          Please read the study reminder before pushing the Start button to
+          start running the Insights Agent
+        </li>
+        <li>
+          If you have received an error message or if you are having trouble,
+          please contact us at 800-XXX-XXXX or email us at
+          help@insightsagent.com
+        </li>
       </ol>
     </>
-  )
+  );
 }
--- src/components/pages/DownloadPage.jsx
+++ src/components/pages/DownloadPage.jsx
@@ -4,28 +4,22 @@ import { DOWNLOAD_URL } from 'constants/urls'
 function DownloadPage() {
   return (
     <main>
-      <div>
+      <div className="bg-[#37770D ]">
         <h1>Getting the App</h1>
-        <a href={DOWNLOAD_URL}>
-          Download for Mac
-        </a>
+        <a href={DOWNLOAD_URL}>Download for Mac</a>
       </div>
 
-      <div id='installation'>
-        <h3>
-          Installation Instructions
-        </h3>
+      <div id="installation">
+        <h3>Installation Instructions</h3>
         <Instructions />
       </div>
 
-      <div id='uninstall'>
-        <h3>
-          Uninstall Instructions
-        </h3>
+      <div id="uninstall">
+        <h3>Uninstall Instructions</h3>
         <Instructions />
       </div>
     </main>
-  )
+  );
 }
 
 export default DownloadPage;
