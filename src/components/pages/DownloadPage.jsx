c0bec60b7b843372d648a46144811c9fe303392d
najwanan <66931010+najwanan@users.noreply.github.com>
2022-12-02 14:35:12 -0800

    fixed padding and margin on button, swapped download button for svg

--- src/components/pages/DownloadPage.jsx
+++ src/components/pages/DownloadPage.jsx
@@ -1,15 +1,15 @@
 import Instructions from 'components/elements/Instructions'
 import { DOWNLOAD_URL } from 'constants/urls';
-import downloadButton from "../../images/vectorDownArrow.png";
+import downloadButton from "../../images/download-arrow.svg";
 
 
 function DownloadPage() {
   return (
     <main>
       <div className="flex flex-col items-center  mt-12">
-        <button className="invisible lg:visible w-350 p-4 rounded-small bg-[#37770D] border-black border-2 flex">
-          <img src={downloadButton} className="px-2"/>
-          <a className="text-white" href={DOWNLOAD_URL}>
+        <button className="invisible lg:visible w-350 p-4 rounded bg-[#37770D] border-black border-2 flex">
+          <img src={downloadButton} className="pr-4"/>
+          <a className="text-white text-extrabold" href={DOWNLOAD_URL}>
             Download for Mac
           </a>
         </button>
