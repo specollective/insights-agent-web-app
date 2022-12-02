32f02a2d1cef4de664dbf35f4ae939c060338272
najwanan <66931010+najwanan@users.noreply.github.com>
2022-12-02 13:19:42 -0800

    changing visibility for download button in mobile

--- src/components/pages/DownloadPage.jsx
+++ src/components/pages/DownloadPage.jsx
@@ -5,7 +5,7 @@ function DownloadPage() {
   return (
     <main>
       <div className="flex flex-col items-center  mt-12">
-        <button className="w-350 p-4 rounded-small bg-[#37770D] border-black border-2">
+        <button className="invisible lg:visible w-350 p-4 rounded-small bg-[#37770D] border-black border-2">
           <a className="text-white" href={DOWNLOAD_URL}>
             Download for Mac
           </a>
