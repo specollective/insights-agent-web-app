8fb8e44f937f2abdf4964958d88bc86c0b260182
najwanan <66931010+najwanan@users.noreply.github.com>
2022-12-02 14:05:34 -0800

    fixing margins and padding, added placeholder for icon

--- src/components/elements/Instructions.jsx
+++ src/components/elements/Instructions.jsx
@@ -1,25 +1,28 @@
 import { loremIpsum } from 'react-lorem-ipsum'
+import logoPlaceHolder from '../../images/ellipse61.png'
+
 
 export default function Instructions() {
   return (
     <>
       <ol className="mt-12 text-lg center">
         {/*add numbers*/}
-        <li className="mt-10 mx-10 px-6 py-2 list-decimal">
+        <li className="mt-6 mx-10 px-6 py-2 list-decimal">
           Click Insights Agent icon on your desktop
+           <img src={logoPlaceHolder}/>
         </li>
-        <li className="mt-8 mx-10 px-6 py-2 list-decimal">
+        <li className="mt-6 mx-10 px-6 py-2 list-decimal">
           Enter your phone number that you used to sign up for this study
         </li>
-        <li className="mt-8 mx-10 px-6 py-2 list-decimal">
+        <li className="mt-6 mx-10 px-6 py-2 list-decimal">
           You will receive a 6 digit verification code via text to that phone
           number provided. Enter that code on the prompt
         </li>
-        <li className="mt-8 mx-10 px-6 py-2 list-decimal">
+        <li className="mt-6 mx-10 px-6 py-2 list-decimal">
           Please read the study reminder before pushing the Start button to
           start running the Insights Agent
         </li>
-        <li className=" mt-8 mx-10 px-6 py-2 list-decimal">
+        <li className=" mt-6 mx-10 px-6 py-2 list-decimal">
           If you have received an error message or if you are having trouble,
           please contact us at 800-XXX-XXXX or email us at
           help@insightsagent.com
--- src/components/pages/DownloadPage.jsx
+++ src/components/pages/DownloadPage.jsx
@@ -1,10 +1,13 @@
 import Instructions from 'components/elements/Instructions'
-import { DOWNLOAD_URL } from 'constants/urls'
+import { DOWNLOAD_URL } from 'constants/urls';
+import downloadButton from "../../images/vectorDownArrow.png";
+import circle from "images/logo.svg"
 
 function DownloadPage() {
   return (
     <main>
       <div className="flex flex-col items-center  mt-12">
+        <img src={circle} />
         <button className="invisible lg:visible w-350 p-4 rounded-small bg-[#37770D] border-black border-2">
           <a className="text-white" href={DOWNLOAD_URL}>
             Download for Mac
@@ -13,14 +16,14 @@ function DownloadPage() {
       </div>
 
       <div className="mt-28" id="installation">
-        <h3 className="center header font-medium text-2xl">
+        <h3 className="center header font-medium text-xl">
           Set-Up Instructions
         </h3>
         <Instructions />
       </div>
 
       <div className="mt-6 mr-6" id="uninstall">
-        <h3 className="center header font-medium text-2xl">
+        <h3 className="center header font-medium text-xl">
           Uninstall Instructions
         </h3>
         <Instructions />
