1453eb57ea245159e899969b4379864d2c9172d6
Yoline <92892101+YolineB@users.noreply.github.com>
2022-12-02 14:16:12 -0800

    Actually fix fragments

--- src/components/elements/HeroMobile.jsx
+++ src/components/elements/HeroMobile.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default function HeroMobile() {
   return (
-    <>
       <section className="flex justify-between items-end h-32 mb-9 pb-3 pl-5 pr-2 bg-green-100">
         <h1 className="text-3xl text-white">
           build <br/> 
@@ -10,6 +9,5 @@ export default function HeroMobile() {
         </h1>
         <h2 className="text-xl font-normal">Insights Agent Study</h2>
       </section>
-    </>
   )
 };
