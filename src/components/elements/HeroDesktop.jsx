8acb5134ec2965f9bec8e124ec9743f94fb0fdcc
Yoline <92892101+YolineB@users.noreply.github.com>
2022-12-02 09:52:59 -0800

    Fix fragments

--- src/components/elements/HeroDesktop.jsx
+++ src/components/elements/HeroDesktop.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
 
-const heroStyle = `
-  flex 
-  justify-between 
-  items-end 
-  h-48
-  mb-20
-  pb-5 
-  pl-20
-  pr-32  
-  bg-green-100
-`
-
 export default function HeroDesktop() {
   return (
     <>
-      <div className={heroStyle}>
+      <section className="flex justify-between items-end h-48 mb-20 pb-5 pl-20 pr-32 bg-green-100">
         <h1 className="text-5xl text-white">
           build <br/> 
           JUSTLY
         </h1>
         <h2 className="text-4xl font-medium">Insights Agent Study</h2>
-      </div>
+      </section>
     </>
   )
 }; 
--- src/components/elements/HeroMobile.jsx
+++ src/components/elements/HeroMobile.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
 
-const heroStyle = `
-  flex 
-  justify-between 
-  items-end 
-  h-32 
-  mb-9 
-  pb-3 
-  pl-5 
-  pr-2 
-  bg-green-100
-`
-
 export default function HeroMobile() {
   return (
     <>
-      <div className={heroStyle}>
+      <section className="flex justify-between items-end h-32 mb-9 pb-3 pl-5 pr-2 bg-green-100">
         <h1 className="text-3xl text-white">
           build <br/> 
           JUSTLY
         </h1>
         <h2 className="text-xl font-normal">Insights Agent Study</h2>
-      </div>
+      </section>
     </>
   )
 };
