aa277250c4419430a44be7a16db23e0605434a1c
najwanan <66931010+najwanan@users.noreply.github.com>
2022-12-03 09:39:59 -0800

    fixing margins

--- src/components/pages/DownloadPage.jsx
+++ src/components/pages/DownloadPage.jsx
@@ -7,7 +7,7 @@ import downloadButton from "../../images/download-arrow.svg";
 function DownloadPage() {
   return (
     <main>
-      <div className="flex flex-col items-center  mt-12">
+      <div className="flex flex-col items-center  mt-40">
         <button className="invisible lg:visible w-350 p-4 rounded bg-[#37770D] border-black border-2 flex">
           <img src={downloadButton} className="pr-4"/>
           <a className="text-white text-extrabold" href={DOWNLOAD_URL}>
@@ -16,15 +16,15 @@ function DownloadPage() {
         </button>
       </div>
 
-      <div className="mt-28" id="installation">
-        <h3 className="center header font-medium text-xl">
+      <div className="mt-24" id="installation">
+        <h3 className="center header font-medium text-2xl">
           Set-Up Instructions
         </h3>
         <Instructions />
       </div>
 
-      <div className="mt-6 mr-6" id="uninstall">
-        <h3 className="center header font-medium text-xl">
+      <div className="mt-40 mr-6" id="uninstall">
+        <h3 className="center header font-medium text-2xl">
           Uninstall Instructions
         </h3>
         <InstructionsUninstall />
