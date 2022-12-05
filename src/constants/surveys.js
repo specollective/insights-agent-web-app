9a7bb61a1a0de9fc865ce2e5ddce463177af5511
Saige Cross <saigecrossdev@gmail.com>
2022-12-05 10:19:14 -0600

    Added columns to survey.

--- src/constants/surveys.js
+++ src/constants/surveys.js
@@ -4,43 +4,45 @@ export const DEFAULT_FORM_VALUES = {
   technologyCompetencyLevel: null,
   race: [],
   householdMembers: '1',
-  computerUse: [], 
+  computerUse: [],
   householdComputers: '0',
 }
 
 // TODO: Apply i18n localization
 export const RACE_OPTIONS = [
-  { value: 'white', label: 'White'},
-  { value: 'black', label: 'Black or African American'},
-  { value: 'american-indian', label: 'American Indian or Alaska Native'},
-  { value: 'chinese', label: 'Chinese'},
-  { value: 'filipino', label: 'Filipino'},
-  { value: 'asian-indian', label: 'Asian Indian'},
-  { value: 'vietnamese', label: 'Vietnamese'},
-  { value: 'korean', label: 'Korean'},
-  { value: 'japanese', label: 'Japanese'},
-  { value: 'hawaiian', label: 'Native Hawaiian'},
-  { value: 'samoan', label: 'Samoan'},
-  { value: 'chamorro', label: 'Charmorro'},
-  { value: 'other-asian', label: 'Other Asian'},
-  { value: 'other-pacific-islander', label: 'Other Pacific Islander'},
-  { value: 'other', label: 'Other race not listed above'},
-  { value: 'decline', label: 'Decline to identify'},
+  { value: 'white', label: 'White' },
+  { value: 'black', label: 'Black or African American' },
+  { value: 'american-indian', label: 'American Indian or Alaska Native' },
+  { value: 'chinese', label: 'Chinese' },
+  { value: 'filipino', label: 'Filipino' },
+  { value: 'asian-indian', label: 'Asian Indian' },
+  { value: 'vietnamese', label: 'Vietnamese' },
+  { value: 'korean', label: 'Korean' },
+]
+export const RACE_OPTIONS_TWO = [
+  { value: 'japanese', label: 'Japanese' },
+  { value: 'hawaiian', label: 'Native Hawaiian' },
+  { value: 'samoan', label: 'Samoan' },
+  { value: 'chamorro', label: 'Charmorro' },
+  { value: 'other-asian', label: 'Other Asian' },
+  { value: 'other-pacific-islander', label: 'Other Pacific Islander' },
+  { value: 'other', label: 'Other race not listed above' },
+  { value: 'decline', label: 'Decline to identify' },
 ]
 
 // TODO: Apply i18n localization
 export const IS_HISPANIC_OPTIONS = [
   { value: 'true', label: 'Yes' },
   { value: 'false', label: 'No' },
-  { value: 'decline', label: 'Decline to identify' }
+  { value: 'decline', label: 'Decline to identify' },
 ]
 
 export const INTERNET_ACCESS = [
-  {value: 'cellular-data', label:'Cellular data plan for smartphone'},
-  {value: 'broadband', label:'Broadband internet service'},
-  {value: 'satellite', label:'Satellite internet service'},
-  {value: 'dial-up', label:'Dial up internet service'},
-  {value: 'other-service', label:'Some other service'},
+  { value: 'cellular-data', label: 'Cellular data plan for smartphone' },
+  { value: 'broadband', label: 'Broadband internet service' },
+  { value: 'satellite', label: 'Satellite internet service' },
+  { value: 'dial-up', label: 'Dial up internet service' },
+  { value: 'other-service', label: 'Some other service' },
 ]
 
 export const HOUSEHOLD_MEMBERS = [
@@ -52,14 +54,14 @@ export const HOUSEHOLD_MEMBERS = [
 ]
 
 export const COMPUTER_USE = [
-  {value: 'entertainment', label: 'Entertainment'},
-  {value: 'gaming', label: 'Gaming'},
-  {value: 'school', label: 'School'},
-  {value: 'creativity', label: 'Creativity'},
-  {value: 'business', label: 'Business'},
-  {value: 'family', label: 'Family'},
-  {value: 'gain-new-skills', label: 'Gain new skills'},
-  {value: 'job-search', label: 'Job search'},
+  { value: 'entertainment', label: 'Entertainment' },
+  { value: 'gaming', label: 'Gaming' },
+  { value: 'school', label: 'School' },
+  { value: 'creativity', label: 'Creativity' },
+  { value: 'business', label: 'Business' },
+  { value: 'family', label: 'Family' },
+  { value: 'gain-new-skills', label: 'Gain new skills' },
+  { value: 'job-search', label: 'Job search' },
 ]
 
 export const TECHNOLOGY_COMPETENCY_LEVEL_OPTIONS = [
@@ -67,7 +69,6 @@ export const TECHNOLOGY_COMPETENCY_LEVEL_OPTIONS = [
   { value: '2', label: '2', secondaryLabel: 'Slightly' },
   { value: '3', label: '3', secondaryLabel: 'Fairly' },
   { value: '4', label: '4', secondaryLabel: 'Highly' },
-  
 ]
 
 export const HOUSEHOLD_COMPUTERS = [
