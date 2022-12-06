c4ba910140f510256f5d9fd85eca145b073f6dbe
najwanan <66931010+najwanan@users.noreply.github.com>
2022-12-06 14:10:49 -0800

    resolving merge conflicts

--- src/components/elements/HeroDesktop.jsx
+++ src/components/elements/HeroDesktop.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function HeroDesktop() {
   return (
-    <>
+    
       <section className="flex justify-between items-end h-48 pb-5 pl-20 pr-32 bg-green-100">
         <h1 className="text-5xl text-white">
           build <br />
@@ -10,6 +10,6 @@ export default function HeroDesktop() {
         </h1>
         <h2 className="text-4xl font-medium">Insights Agent Study</h2>
       </section>
-    </>
+    
   );
 }
