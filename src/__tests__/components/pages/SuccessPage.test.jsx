c9cda261a33c057c694857796422dfc40ce76249
Yoline <92892101+YolineB@users.noreply.github.com>
2022-12-07 11:50:15 -0800

    test fix test

--- src/__tests__/components/pages/SuccessPage.test.jsx
+++ src/__tests__/components/pages/SuccessPage.test.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter as Router } from 'react-router-dom';
 import SuccessPage from 'components/pages/SuccessPage';
 
 it('renders success message', () => {
-  render(<SuccessPage />);
+  render(
+    <Router>
+      <SuccessPage />
+    </Router>
+  );
   expect(screen.getByText('Thank you for providing your data and helping us build the future of technology - for everyone.')).toBeInTheDocument();
 });
