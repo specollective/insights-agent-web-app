70a9f5e07b6a98729fa0321a07f80ff17aa69281
Yoline <92892101+YolineB@users.noreply.github.com>
2022-12-07 12:21:23 -0800

    Issue 47: Survey Confirmation Page Styling (#64)
    * Comment auth to edit
    
    * style:Header styling and padding
    
    * test: Corrected test text
    
    * style: Unordered list and wording
    
    * test: Correct text for test
    
    * feat: Add buildJustly hyperlink
    
    * feat:Add back to home link
    
    * style:Fix padding
    
    * test fix test
    
    * feat:Open link in new window
    
    * Uncomment auth
    
    * style:Correct alignment
    
    * Remove unused className

--- src/components/pages/SuccessPage.jsx
+++ src/components/pages/SuccessPage.jsx
@@ -1,15 +1,34 @@
-import './SuccessPage.css'
+import { Link } from 'react-router-dom'
 
 function SuccessPage() {
   return (
-    <main className="text-center">
-      <div className="container">
-        <h1>Thank You!</h1>
-        <p>Your survey has been submitted</p>
-        <p>
-          Next, you should receive text message with instructions to open, download and install the Insights Agent
-        </p>
+    <main className="px-10 md:px-40 xl:px-60 py-12 text-center">
+      <h1 className='pb-7 md:pb-12 text-base md:text-3xl font-semibold'>
+        Thank you for providing your data and helping us build the future of technology - for everyone.
+      </h1>
+
+      <ul className='pl-8 md:pl-14 pb-12 list-disc text-left space-y-8 '>
+        <li>
+          We are processing your information now and we’ll notify you with next steps.
+        </li>
+        <li>
+          You’ll receive a text message at the number you provided.
+        </li>
+        <li>
+          If you do not receive a message within 7 days, please send an email to tech4all@buildjustly.org and mention the Insights Agent.
+        </li>
+      </ul>
+
+      <div>
+        <a href='https://buildjustly.org/' target="_blank" rel="noreferrer" className='text-[#064187] underline md:no-underline md:hover:underline'>
+          Visit buildJUSTLY.org
+        </a>
       </div>
+      
+      <div className='hidden md:block align-right pt-5 text-xl underline'>
+        <Link to='/'>Go back to Home</Link>
+      </div>
+
     </main>
   )
 }
