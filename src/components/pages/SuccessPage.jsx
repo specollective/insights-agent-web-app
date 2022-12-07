d37f1dcfc238f92851db6df3dbb24d8e04056ee6
Yoline <92892101+YolineB@users.noreply.github.com>
2022-12-07 09:07:38 -0800

    style:Header styling and padding

--- src/components/pages/SuccessPage.jsx
+++ src/components/pages/SuccessPage.jsx
@@ -1,10 +1,11 @@
-import './SuccessPage.css'
 
 function SuccessPage() {
   return (
-    <main className="text-center">
-      <div className="container">
-        <h1>Thank You!</h1>
+    <main className="px-8 md:px-40 pt-12 md:pt-0 text-center">
+      <div className="">
+        <h1 className='pb-7 md:pb-12 text-base md:text-3xl font-semibold'>
+          Thank you for providing your data and helping us build the future of technology - for everyone.
+        </h1>
         <p>Your survey has been submitted</p>
         <p>
           Next, you should receive text message with instructions to open, download and install the Insights Agent
