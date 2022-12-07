0337acaf7798fd57534f240f4a2556754e3691d6
Yoline <92892101+YolineB@users.noreply.github.com>
2022-12-07 11:18:41 -0800

    style:Fix padding

--- src/components/elements/HeroDesktop.jsx
+++ src/components/elements/HeroDesktop.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HeroDesktop() {
   return (
-      <section className='flex justify-between items-end h-48 mb-20 pb-5 pl-20 pr-32 bg-green-100'>
+      <section className='flex justify-between items-end h-48 pb-5 pl-20 pr-32 bg-green-100'>
         <h1 className='text-5xl text-white'>
           build <br/> 
           JUSTLY
