dafef945492b857631f12e48e9ced46826909140
Yoline <92892101+YolineB@users.noreply.github.com>
2022-12-07 11:18:24 -0800

    feat:Add back to home link

--- src/components/pages/SuccessPage.jsx
+++ src/components/pages/SuccessPage.jsx
@@ -1,7 +1,8 @@
+import { Link } from 'react-router-dom'
 
 function SuccessPage() {
   return (
-    <main className="px-10 md:px-40 pt-12 md:pt-0 text-center">
+    <main className="px-10 md:px-40 xl:px-60 py-12 text-center">
       <h1 className='pb-7 md:pb-12 text-base md:text-3xl font-semibold'>
         Thank you for providing your data and helping us build the future of technology - for everyone.
       </h1>
@@ -18,9 +19,15 @@ function SuccessPage() {
         </li>
       </ul>
 
-      <a href='https://buildjustly.org/' className='text-[#064187] underline md:no-underline md:hover:underline'>
-        Visit buildJUSTLY.org
-      </a>
+      <div>
+        <a href='https://buildjustly.org/' className='text-[#064187] underline md:no-underline md:hover:underline'>
+          Visit buildJUSTLY.org
+        </a>
+      </div>
+      
+      <div className='hidden md:block float-right pt-5 text-xl underline'>
+        <Link to='/' className=''>Go back to Home</Link>
+      </div>
 
     </main>
   )
