7a7913e461228bda421d62f59da126da562f2e03
Yoline <92892101+YolineB@users.noreply.github.com>
2022-12-07 09:54:19 -0800

    style: Unordered list and wording

--- src/components/pages/SuccessPage.jsx
+++ src/components/pages/SuccessPage.jsx
@@ -1,16 +1,23 @@
 
 function SuccessPage() {
   return (
-    <main className="px-8 md:px-40 pt-12 md:pt-0 text-center">
-      <div className="">
-        <h1 className='pb-7 md:pb-12 text-base md:text-3xl font-semibold'>
-          Thank you for providing your data and helping us build the future of technology - for everyone.
-        </h1>
-        <p>Your survey has been submitted</p>
-        <p>
-          Next, you should receive text message with instructions to open, download and install the Insights Agent
-        </p>
-      </div>
+    <main className="px-10 md:px-40 pt-12 md:pt-0 text-center">
+      <h1 className='pb-7 md:pb-12 text-base md:text-3xl font-semibold'>
+        Thank you for providing your data and helping us build the future of technology - for everyone.
+      </h1>
+
+      <ul className='pl-8 md:pl-14 pb-12 list-disc text-left space-y-8 '>
+        <li>
+          We are processing your information now and we’ll notify you with next steps.
+        </li>
+        <li>
+          You’ll receive a text message at the number you provided.
+        </li>
+        <li>
+          If you do not receive a message within 7 days, please send an email to tech4all@buildjustly.org and mention the Insights Agent.
+        </li>
+      </ul>
+
     </main>
   )
 }
