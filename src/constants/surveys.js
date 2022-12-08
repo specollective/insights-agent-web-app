8c8661a77f8253e120f8c640cb1cce65d23b6588
Saige Cross <saigecrossdev@gmail.com>
2022-12-08 16:09:54 -0600

    reverting.

--- src/constants/surveys.js
+++ src/constants/surveys.js
@@ -47,7 +47,7 @@ export const INTERNET_ACCESS = [
 ]
 
 export const HOUSEHOLD_MEMBERS = [
-  // { value: '0', label: '0' },
+  { value: '0', label: '0' },
   { value: '1', label: '1' },
   { value: '2', label: '2' },
   { value: '3', label: '3' },
