8476230a14a8010785a7681d9371214d2b5b9ccd
Yoline <92892101+YolineB@users.noreply.github.com>
2022-12-13 12:55:22 -0800

    Add hero mobile, correct white space

--- src/components/pages/DownloadPage.jsx
+++ src/components/pages/DownloadPage.jsx
@@ -8,9 +8,12 @@ import downloadButton from "../../images/download-arrow.svg";
 function DownloadPage() {
   return (
     <main>
+      <div className="visible md:hidden">
+        <HeroMobile />
+      </div>
       <h1 className="sr-only">Download Page</h1>
     
-      <section className="flex flex-col items-center mt-40">
+      <section className="hidden lg:flex flex-col items-center mt-40">
         <button className="hidden lg:flex p-4 rounded border-black border-2 bg-[#37770D] w-350">
           <img src={downloadButton} className="pr-4" alt="download image arrow pointing down" />
           <a className="text-white text-extrabold" href={DOWNLOAD_URL}>
