a9c7987582b5e739d156fc2a772f82742ab8cf63
Saige Cross <saigecrossdev@gmail.com>
2022-12-15 10:49:32 -0600

    Added just mock. Changing to draft pr.

--- src/__tests__/components/pages/SurveyPage.test.jsx
+++ src/__tests__/components/pages/SurveyPage.test.jsx
@@ -27,6 +27,19 @@ jest.mock('react-router-dom', () => ({
   useParams: () => jest.fn(),
 }));
 
+// Mock out react i18next
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+}));
+
 // Helper function to DRY up the test code.
 function renderPage() {
   render(
