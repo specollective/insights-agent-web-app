47bb3eb9de91d2f9592f3c6bfeb1abf88cf3f464
Saige Cross <saigecrossdev@gmail.com>
2022-12-15 08:14:22 -0600

    Success page finished.

--- src/components/pages/SuccessPage.jsx
+++ src/components/pages/SuccessPage.jsx
@@ -1,36 +1,39 @@
 import { Link } from 'react-router-dom'
 import ProgressBar2 from '../../images/ProgressBar2.svg'
+import { useTranslation } from 'react-i18next'
 
 function SuccessPage() {
+  const { t } = useTranslation()
+
   return (
     <main className="px-10 md:px-40 xl:px-60 py-12 text-center">
       <div className='grid place-content-center pb-8 md:pt-8 md:pb-16'>
         <img src={ProgressBar2} alt='progress' />
       </div>
       <h1 className='pb-7 md:pb-12 text-base md:text-3xl font-semibold'>
-        Thank you for providing your data and helping us build the future of technology - for everyone.
+        {t("SuccessHeader")}
       </h1>
 
       <ul className='pl-8 md:pl-14 pb-12 list-disc text-left space-y-8 '>
         <li>
-          We are processing your information now and we’ll notify you with next steps.
+          {t("SuccessProcessing")}
         </li>
         <li>
-          You’ll receive a text message at the number you provided.
+          {t("SuccessTextMessage")}
         </li>
         <li>
-          If you do not receive a message within 7 days, please send an email to tech4all@buildjustly.org and mention the Insights Agent.
+          {t("SuccessIfYouDoNotReceive")}
         </li>
       </ul>
 
       <div>
         <a href='https://buildjustly.org/' target="_blank" rel="noreferrer" className='text-[#064187] underline md:no-underline md:hover:underline'>
-          Visit buildJUSTLY.org
+          {t("SuccessLink")}
         </a>
       </div>
       
       <div className='hidden md:block align-right pt-5 text-xl underline'>
-        <Link to='/'>Go back to Home</Link>
+        <Link to='/'>{t("ConfirmationGoHome")}</Link>
       </div>
 
     </main>
