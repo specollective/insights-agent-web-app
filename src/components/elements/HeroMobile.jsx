af7a3d3c0e53ca59c5d2098b8219cf0e3ade147a
Saige Cross <saigecrossdev@gmail.com>
2022-12-15 09:07:57 -0600

    added a few more missing translations.

--- src/components/elements/HeroMobile.jsx
+++ src/components/elements/HeroMobile.jsx
@@ -1,13 +1,16 @@
+import { useTranslation } from "react-i18next";
 import React from 'react'
 
 export default function HeroMobile() {
+  const { t } = useTranslation()
+
   return (
       <section className='flex justify-between items-end h-32 mb-9 pb-3 pl-5 pr-2 bg-green-100'>
         <h1 className='text-3xl text-white'>
           build <br/> 
           JUSTLY
         </h1>
-        <h2 className='text-xl font-normal'>Insights Agent Study</h2>
+        <h2 className='text-xl font-normal'>{t("InsightsAgentStudy")}</h2>
       </section>
   )
 };
--- src/components/pages/DownloadPage.jsx
+++ src/components/pages/DownloadPage.jsx
@@ -3,9 +3,13 @@ import Instructions from 'components/elements/Instructions'
 // import InstructionsUninstall from 'components/elements/InstructionsUninstall'
 import { DOWNLOAD_URL } from 'constants/urls';
 import downloadButton from "../../images/download-arrow.svg";
+import { useTranslation } from 'react-i18next'
+
 
 
 function DownloadPage() {
+  const { t } = useTranslation()
+
   return (
     <main>
       <div className="visible md:hidden">
@@ -17,14 +21,14 @@ function DownloadPage() {
         <button className="hidden lg:flex p-4 rounded border-black border-2 bg-[#37770D] w-350">
           <img src={downloadButton} className="pr-4" alt="download image arrow pointing down" />
           <a className="text-white text-extrabold" href={DOWNLOAD_URL}>
-            Download for Mac
+            {t("DownloadMac")}
           </a>
         </button>
       </section>
 
       <section className="mt-24">
         <h2 className="header font-medium text-2xl">
-          Set-Up Instructions
+          {t("DownloadSetUp")}
         </h2>
         <Instructions />
       </section>
@@ -32,7 +36,7 @@ function DownloadPage() {
       {/* Todo: Add uninstall instructions */}
       {/* <section className="mt-40 mr-6">
         <h2 className="center header font-medium text-2xl">
-          Uninstall Instructions
+        {t("DownloadUninstall")}
         </h2>
         <InstructionsUninstall />
       </section> */}
