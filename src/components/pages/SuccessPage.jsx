3f09edd476c767e401ae74e20783c55fcf29e8eb
Yoline <92892101+YolineB@users.noreply.github.com>
2023-01-10 15:28:53 -0800

    Add translations for success page, and spanish progress bar img

--- src/components/pages/SuccessPage.jsx
+++ src/components/pages/SuccessPage.jsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom'
 import ProgressBar2 from '../../images/ProgressBar2.svg'
+import ProgressBar2ES from '../../images/ProgressBar2ES.svg'
 import { useTranslation } from 'react-i18next'
 
 function SuccessPage() {
   const { t } = useTranslation()
+  const lang = localStorage.getItem('locale')
 
   return (
     <main className="px-10 md:px-40 xl:px-60 py-12 text-center">
       <div className='grid place-content-center pb-8 md:pt-8 md:pb-16'>
-        <img src={ProgressBar2} alt='progress' />
+       { lang === 'en' ? <img src={ProgressBar2} alt='progress' /> : <img src={ProgressBar2ES} alt='progressES' />}
       </div>
       <h1 className='pb-7 md:pb-12 text-base md:text-3xl font-semibold'>
         {t("SuccessHeader")}
