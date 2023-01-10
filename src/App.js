7ea3e3417d30c299708d2bdb3f1dedea2643eff9
Yoline <92892101+YolineB@users.noreply.github.com>
2023-01-10 15:29:41 -0800

    Un-comment success page auth

--- src/App.js
+++ src/App.js
@@ -56,9 +56,9 @@ function App() {
             <Route
               path="/success"
               element={
-                // <RequireAuth>
+                <RequireAuth>
                   <SuccessPage />
-                // </RequireAuth>
+                </RequireAuth>
               }
             />
 
